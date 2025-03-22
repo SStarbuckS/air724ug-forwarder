a37c40b4244b5df4e4084cc0a7a3f91eac3c68e0
SStarbuckS <22103918+SStarbuckS@users.noreply.github.com>
2025-03-22 17:01:02 +0800

    Update util_uptime.lua

--- script/utils/util_uptime.lua
+++ script/utils/util_uptime.lua
@@ -1,11 +1,9 @@
 module(..., package.seeall)
 
--- GET请求发送心跳包
+-- 发送心跳包（使用 GET 请求）
 function send_heartbeat()
     log.info("util_uptime", "开始发送心跳包")
 
     -- 直接发送请求，不处理回调
-    sys.taskInit(function()
-        http.request("GET", config.HEARTBEAT_URL)
-    end)
+    http.request("GET", config.HEARTBEAT_URL)
 end
